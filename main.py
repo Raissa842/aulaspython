cafd2a3fb818ce558f3c040db49c19bb49567bef
Raissa842 <raissamourasilva2007@gmail.com>
2025-06-07 14:19:16 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+  nomeEscola = "Carlos Alberto"
+  idade = 17
+  tamanho = 1.55
+  sexo = "M"
+
+  print (nomeEscola)
+  print (idade)
+  print (tamanho)
+  print (sexo)
+  
+  return 0
+main()
