b6faac7f0a82b6496e03df618eb08502ae3a683f
Raissa842 <raissamourasilva2007@gmail.com>
2025-06-07 14:32:05 -0300

    Add files via upload

---
+++ leitura.py
@@ -0,0 +1,12 @@
+def main():
+  nomeEscola = input ("Digite o nome da escola: ")
+  idade = int (input ("Digite a idade: ") )
+  tamanho = float (input ("Digite o tamanho: "))
+  sexo = input ("Digite o sexo: (M/F/N)")
+
+  print (nomeEscola)
+  print (idade)
+  print (tamanho)
+  print (sexo) 
+  return 0
+main()
